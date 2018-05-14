f172b996157a8ed8202e00192f68bda9d7f7de0a
hotdogwizard <grunt.slyer@gmail.com>
2018-05-14 14:49:53 +0200

    added port and fixed lan hosting

--- base.py
+++ base.py
@@ -171,6 +171,12 @@ def index():
 
     return render_template("message.html", replys=replys)
 
-if __name__ == "__main__":
-    app.run()
+do80 = false
+
+if not do80:
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0")
+else:
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=80)
 
